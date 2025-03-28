6585d11c7cce11dedeccd69e6e167492ebc02b77
Om Bhanuse <OmBhanuse.dypcet@dypgroup.edu.in>
2025-03-28 08:04:09 +0530

    First initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
 
+const app=express();
+
+
+app.use(express.json());
+dotenv.config();    
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
+import './App.css';
+
+function App() {
+    return (
+      <Router>
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+          <Routes>
+            <Route path="/" element={<> <OpdForm /></>} />
+            <Route path="/dashboard" element={<ProtectedRoute><AdminDashboard></AdminDashboard></ProtectedRoute>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
+  
+  export default App;
