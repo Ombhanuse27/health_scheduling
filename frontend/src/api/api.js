2988c7f0ab8dc66c4386411fe5544248508ea689
Om Bhanuse <OmBhanuse.dypcet@dypgroup.edu.in>
2025-06-20 11:28:41 +0530

    Deployment2

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://health-scheduling.onrender.com/api"; // Change if needed
+const BASE_URL = "https://health-scheduling.onrender.com/"; // Change if needed
 
 export const registerAdmin = (data) => axios.post(`${BASE_URL}/admin/register`, data);
 export const loginAdmin = (data) => axios.post(`${BASE_URL}/admin/login`, data);
