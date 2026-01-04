c1d28abc047ca3c36c2cc1890f26ae8a0f297d2d
Ombhanuse27 <ombhanuse364@gmail.com>
2026-01-05 01:51:26 +0530

    env added

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.RENDER_EXTERNAL_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   // baseURL:"http://localhost:5000/api",
   timeout: 60000, // important for Render cold start
 });
